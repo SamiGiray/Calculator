5193c7ef6bca86607ed09991749c637c8bc01213
SamiGiray <97362160+SamiGiray@users.noreply.github.com>
2022-06-21 23:31:34 +0300

    Update ViewController.swift

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,101 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var num1: UITextField!
+    @IBOutlet weak var num2: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var savedResult: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let savedNumber = UserDefaults.standard.object(forKey: "Saved")
+        if let newNumber = savedNumber as? String{
+            savedResult.text = newNumber
+        }
+        
+        
+        
+        
     }
 
 
+    @IBAction func savedButton(_ sender: Any) {
+        
+        
+        UserDefaults.standard.set(savedResult.text!, forKey: "Saved")
+        savedResult.text = resultLabel.text
+          
+    
+        
+        
+    }
+    
+  
+    
+    
+    
+    @IBAction func plus(_ sender: Any) {
+        
+        
+        if let myTextfield = Int(num1.text!){
+            if let myTextfield2 = Int(num2.text!){
+                let result = myTextfield+myTextfield2
+                resultLabel.text = String(result)
+            
+            }else{
+                resultLabel.text = "Hatalı değer girdiniz!"
+            }
+            
+        }
+        
+        
+        
+    }
+    
+    @IBAction func minus(_ sender: Any) {
+        
+        if let myTextfield = Int(num1.text!){
+            if let myTextfield2 = Int(num2.text!){
+                let result = myTextfield-myTextfield2
+                resultLabel.text = String(result)
+            }else{
+                resultLabel.text = "Hatalı değer girdiniz!"
+            }
+        }
+        
+        
+    }
+    
+    @IBAction func divison(_ sender: Any) {
+        
+        if let myTextfield = Int(num1.text!){
+            if let myTextfield2 = Int(num2.text!){
+                let result = myTextfield/myTextfield2
+                resultLabel.text = String(result)
+            }else{
+                resultLabel.text = "Hatalı değer girdiniz!"
+            }
+        }
+        
+    }
+    
+    @IBAction func multiplication(_ sender: Any) {
+        
+        if let myTextfield = Int(num1.text!){
+            if let myTextfield2 = Int(num2.text!){
+                let result = myTextfield*myTextfield2
+                resultLabel.text = String(result)
+            }else{
+                resultLabel.text = "Hatalı değer girdiniz!"
+            }
+        }
+        
+    }
+    
 }
 
